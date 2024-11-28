1be372fbeb7efb3aad0e8f9be0ea724883c4844c
Juan Mata <91892835+juanmata06@users.noreply.github.com>
2024-11-28 17:44:13 +0100

    fix for enviroments hidden

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
 })
-export class RegisterComponent implements OnInit {
+export class LoginComponent implements OnInit {
   /**
   * ------------------------------------------------------------------------------------------------------------------------------
   * General vars for component
   * ------------------------------------------------------------------------------------------------------------------------------
   */
+ 
   loading: boolean = false;
   formGroup: FormGroup;
+
   /**
    * -----------------------------------------------------------------------------------------------------------------------------
    * LYFECYCLE METHODS
@@ -21,18 +25,17 @@ export class RegisterComponent implements OnInit {
    */
 
   constructor(
-    private _formBuilder: FormBuilder
+    private _formBuilder: FormBuilder,
+    private _router: Router,
+    private _authService: AuthService,
   ) {
-    this.loading = true;
-  }
-
-  ngOnInit(): void {
     this.formGroup = this._formBuilder.group({
-      name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
-    }); 
+    });
   }
+
+  ngOnInit(): void {}
 
   /**
   * ------------------------------------------------------------------------------------------------------------------------------
@@ -51,9 +54,23 @@ export class RegisterComponent implements OnInit {
   * PUBLIC METHODS
   * ------------------------------------------------------------------------------------------------------------------------------
   */
-  public submitFormGroup(): void{
-    console.log(this.formGroup.value);
+
+  public submitFormGroup(): void {
+    if (this.formGroup.invalid) { return; }
+    const { email, password } = this.formGroup.value;
+    console.log(email, password);
+    
+    this._authService.loginUser(email, password)
+      .then(
+        (response: any) => {
+          console.log(response);
+          this._router.navigate(['/'])
+
+        }
+      )
+      .catch((err: any) => console.error(err))
   }
+
   /**
   * ------------------------------------------------------------------------------------------------------------------------------
   * PUBLIC VALIDATION AND INTERNAL PROCESS METHODS
