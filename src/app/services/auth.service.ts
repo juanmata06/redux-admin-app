d5d027b3a896e375c4e64cb77b64255fcf6d1962
Juan Mata <91892835+juanmata06@users.noreply.github.com>
2024-12-02 14:19:10 +0100

    using firestore database

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Observable, map } from 'rxjs';
+import { iUser } from '../interfaces/user.interface';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -10,25 +12,35 @@ export class AuthService {
 
   constructor(
     private _router: Router,
-    private _angularFireAuth: AngularFireAuth
+    private _angularFireAuth: AngularFireAuth,
+    private _angularFirestore: AngularFirestore
   ) { }
 
-  public initAuthListener(): any {
+  public initAuthListener(): void {
     this._angularFireAuth.authState.subscribe((user: any) => {
       console.log(user.uid, user.email);
 
     });
   }
 
-  public createUser(name: string, email: string, password: string): any {
-    return this._angularFireAuth.createUserWithEmailAndPassword(email, password);
+  public createUser(name: string, email: string, password: string): Promise<void> {
+    return this._angularFireAuth.createUserWithEmailAndPassword(email, password).then(({ user }) => {
+
+      const newUser: iUser = {
+        uid: user?.uid,
+        name: name,
+        email: user?.email || undefined,
+      };
+
+      return this._angularFirestore.doc(`${user?.uid}/user`).set(newUser);
+    });
   }
 
-  public logIn(email: string, password: string): any {
+  public logIn(email: string, password: string): Promise<any> {
     return this._angularFireAuth.signInWithEmailAndPassword(email, password);
   }
 
-  public logOut(): any {
+  public logOut(): Promise<void> {
     return this._angularFireAuth.signOut();
   }
 
